341442fff3de89ca23242c686c6f90ced59af382
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-05-16 18:44:44 +0200

    Header
    Adding header

---
+++ src/components/MeImage.js
@@ -0,0 +1,11 @@
+import Me from "./images/glaucia.png";
+
+const ImageDisplay = () => {
+    return (
+    <div>
+         <img className="image" src={Me} alt="Gláucia's image"/> 
+    </div>
+    );  
+};
+
+export default ImageDisplay;
